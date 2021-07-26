618160e0c3218d0d38e6d4a876653f2c285e2094
gonnabepartofbtsconcert <esaadilla12@gmail.com>
2021-07-26 10:57:53 +0700

    First commit

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * array adalah kumpulan nilai yang berjanis sama
+ * setiap data punya posisi yang disebut index (start 0)
+ * 
+ * variabel -> individu
+ * array -> populasi
+ */ 
+ // array digunakan saat ada data yang bisa dikeompokkan
+// array numeric -> array yang indexnya berupa numerik
+// array assosiative -> array yang indexnya berupa string
+ 
+$student = ""
+?>
